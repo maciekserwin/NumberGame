74f097cf55bcce8e9b9c412eb31268e9128292b3
maciekserwin <maciekserwin@gmail.com>
2023-06-29 21:28:06 +0200

    create random generator

---
+++ src/NumberGame.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 100) +1;
+        System.out.println("I have randomly chosen a number between 1 and 100.");
+        System.out.println("Try to guess it.");
+    }
+}
