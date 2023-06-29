cebc5459a7fb330a9dee4b9d72fa604365a1e041
maciekserwin <maciekserwin@gmail.com>
2023-06-29 21:49:57 +0200

    add first case randomNumber > guess

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class NumberGame {
     public static void main(String[] args) {
         int randomNumber = (int) (Math.random() * 100) +1;
+        boolean hasWon = false;
+
         System.out.println("I have randomly chosen a number between 1 and 100.");
         System.out.println("Try to guess it.");
 
         Scanner scanner = new Scanner(System.in);
-        for (int i = 10; i>0; i--) {
+        for (int i = 10; i > 0; i--) {
             System.out.println("You have " + i + " guess(es) left. Choose again: ");
             int guess = scanner.nextInt();
-            System.out.println("Your guess was: " +guess);
+
+            if (randomNumber < guess) {
+                System.out.println("It's smaller than " + guess + "guess.");
+            }
         }
     }
 }
