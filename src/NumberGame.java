5e8f08801e296e7de7e583fef794cae39a107c34
maciekserwin <maciekserwin@gmail.com>
2023-06-29 22:00:03 +0200

    add third case randomNumber = guess with loose

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class NumberGame {
     public static void main(String[] args) {
-        int randomNumber = (int) (Math.random() * 100) +1;
+        int randomNumber = (int) (Math.random() * 100) + 1;
         boolean hasWon = false;
 
         System.out.println("I have randomly chosen a number between 1 and 100.");
@@ -24,8 +24,11 @@ public class NumberGame {
                 break;
             }
         }
-     if (hasWon) {
-         System.out.println("CORRECT... YOU WIN!!!");
-     }
+        if (hasWon) {
+            System.out.println("CORRECT... YOU WIN!!!");
+        } else {
+            System.out.println("GAME OVER... YOU LOOSE!!!");
+            System.out.println("The number was : " + randomNumber);
+        }
     }
 }
